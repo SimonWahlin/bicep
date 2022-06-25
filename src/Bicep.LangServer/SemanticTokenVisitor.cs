9bc1b8928f0acc6e4360f205cfd81c736b5b9228
Simon Wahlin <simon.wahlin@gmail.com>
2022-06-25 23:45:01 +0200

    Third commit

--- src/Bicep.LangServer/SemanticTokenVisitor.cs
+++ src/Bicep.LangServer/SemanticTokenVisitor.cs
@@ -289,6 +289,13 @@ namespace Bicep.LanguageServer
             base.VisitVariableAccessSyntax(syntax);
         }
 
+        public override void VisitMetadataDeclarationSyntax(MetadataDeclarationSyntax syntax)
+        {
+            AddTokenType(syntax.Keyword, SemanticTokenType.Keyword);
+            AddTokenType(syntax.Name, SemanticTokenType.Variable);
+            base.VisitMetadataDeclarationSyntax(syntax);
+        }
+
         public override void VisitVariableDeclarationSyntax(VariableDeclarationSyntax syntax)
         {
             AddTokenType(syntax.Keyword, SemanticTokenType.Keyword);
